a065d7bf812bf2bcff2952d010fae0353537cc4c
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2019-11-22 16:03:44 +0100

    first commit

---
+++ C-Sharp Project/CountingSort.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Project
+{
+    public class CountingSort
+    {
+        public static void CountSort(string[] arr)
+        {
+            int arr_length = arr.Length;
+
+            char[] new_arr = new char[arr_length];
+
+            // The output character array that 
+            // will have sorted arr. 
+            string[] output = new string[arr_length];
+
+            // Create a count array to store  
+            // count of inidividual characters  
+            // and initialize count array as 0 
+            int[] count = new int[256];
+
+            Console.WriteLine();
+            for (int i = 0; i < 256; i++)
+            {
+                count[i] = 0;
+            }
+
+            // store count of each character 
+            for (int i = 0; i < arr_length; i++)
+            {
+                new_arr[i] = Convert.ToChar(arr[i].Substring(0, 1));
+
+                //Implicit type conversion from char to int
+                //Possible in both C# and Java.
+                ++count[new_arr[i]];
+            }
+
+            // Change count[i] so that count[i]  
+            // now contains actual position of  
+            // this character in output array 
+            for (int i = 1; i < 256; i++)
+            {
+                count[i] += count[i - 1];
+            }
+
+            // Build the output character array 
+            // To make it stable we are operating in reverse order. 
+            for (int i = arr_length - 1; i >= 0; i--)
+            {
+                output[count[new_arr[i]] - 1] = arr[i];
+                --count[new_arr[i]];
+            }
+
+            // Copy the output array to arr, so 
+            // that arr now contains sorted  
+            // characters 
+            for (int i = 0; i < arr_length; i++)
+            {
+                arr[i] = output[i];
+            }
+        }
+    }
+}
---
+++ DocumentIndexerUnitTesting/TestIndexDocuments.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using C_Sharp_Project;
+using System.Collections.Generic;
+using System.Linq; 
+
+namespace DocumentIndexerUnitTesting
+{
+    [TestClass]
+    public class TestIndexDocuments
+    {
+        [TestMethod]
+        public void TestIndexDocuments1()
+        {
+            Indexer ind_obj = new Indexer();
+
+            string[] documents = {
+                @"C:\Users\Sunkanmi\Documents\1) Uni files\300 level files\2nd Semester\CSC 322(C#)\Project Materials\Random documents\About yourself.txt",
+                @"C:\Users\Sunkanmi\Documents\1) Uni files\300 level files\2nd Semester\CSC 322(C#)\Project Materials\Random documents\Hour glass.docx",
+                @"C:\Users\Sunkanmi\Documents\1) Uni files\300 level files\2nd Semester\CSC 322(C#)\Project Materials\Random documents\Spaghetti.txt",
+                @"C:\Users\Sunkanmi\Documents\1) Uni files\300 level files\2nd Semester\CSC 322(C#)\Project Materials\Random documents\The Dark Knight.docx"
+            };
+                
+            Dictionary<string, Dictionary<string, int>> DocumentIndexes = ind_obj.IndexDocuments(documents);
+
+            Dictionary<string, int> firstDoc = DocumentIndexes.Values.ElementAt(0);
+
+            Assert.AreEqual(firstDoc.Keys.ElementAt(0), "Hi");            
+        }
+
+        [TestMethod]
+        public void TestIndexDocuments2()
+        {
+            Indexer ind_obj = new Indexer();
+
+            string[] documents = {
+                @"C:\Users\Sunkanmi\Documents\1) Uni files\300 level files\2nd Semester\CSC 322(C#)\Project Materials\Random documents\About yourself.txt",
+                @"C:\Users\Sunkanmi\Documents\1) Uni files\300 level files\2nd Semester\CSC 322(C#)\Project Materials\Random documents\Hour glass.docx",
+                @"C:\Users\Sunkanmi\Documents\1) Uni files\300 level files\2nd Semester\CSC 322(C#)\Project Materials\Random documents\Spaghetti.txt",
+                @"C:\Users\Sunkanmi\Documents\1) Uni files\300 level files\2nd Semester\CSC 322(C#)\Project Materials\Random documents\The Dark Knight.docx"
+            };
+
+            Dictionary<string, Dictionary<string, int>> DocumentIndexes = ind_obj.IndexDocuments(documents);
+
+            Dictionary<string, int> secondDoc = DocumentIndexes.Values.ElementAt(1);
+
+            Assert.AreEqual(secondDoc.Keys.ElementAt(1), "glass"); 
+        }
+
+        [TestMethod]
+        public void TestIndexDocuments3()
+        {
+            Indexer ind_obj = new Indexer();
+
+            string[] documents = {
+                @"C:\Users\Sunkanmi\Documents\1) Uni files\300 level files\2nd Semester\CSC 322(C#)\Project Materials\Random documents\About yourself.txt",
+                @"C:\Users\Sunkanmi\Documents\1) Uni files\300 level files\2nd Semester\CSC 322(C#)\Project Materials\Random documents\Hour glass.docx",
+                @"C:\Users\Sunkanmi\Documents\1) Uni files\300 level files\2nd Semester\CSC 322(C#)\Project Materials\Random documents\Spaghetti.txt",
+                @"C:\Users\Sunkanmi\Documents\1) Uni files\300 level files\2nd Semester\CSC 322(C#)\Project Materials\Random documents\The Dark Knight.docx"
+            };
+
+            Dictionary<string, Dictionary<string, int>> DocumentIndexes = ind_obj.IndexDocuments(documents);
+            Dictionary<string, int> thirdDoc = DocumentIndexes.Values.ElementAt(2);
+
+            Assert.AreEqual(thirdDoc.Keys.ElementAt(3), "carbohydrate");
+        }
+
+        [TestMethod]
+        public void TestIndexDocuments4()
+        {
+            Indexer ind_obj = new Indexer();
+
+            string[] documents = {
+                @"C:\Users\Sunkanmi\Documents\1) Uni files\300 level files\2nd Semester\CSC 322(C#)\Project Materials\Random documents\About yourself.txt",
+                @"C:\Users\Sunkanmi\Documents\1) Uni files\300 level files\2nd Semester\CSC 322(C#)\Project Materials\Random documents\Hour glass.docx",
+                @"C:\Users\Sunkanmi\Documents\1) Uni files\300 level files\2nd Semester\CSC 322(C#)\Project Materials\Random documents\Spaghetti.txt",
+                @"C:\Users\Sunkanmi\Documents\1) Uni files\300 level files\2nd Semester\CSC 322(C#)\Project Materials\Random documents\The Dark Knight.docx"
+            };
+
+            Dictionary<string, Dictionary<string, int>> DocumentIndexes = ind_obj.IndexDocuments(documents);
+            Dictionary<string, int> fourthDoc = DocumentIndexes.Values.ElementAt(3);
+
+            Assert.AreEqual(fourthDoc.Keys.ElementAt(1), "dark");
+        }
+
+    }
+}
